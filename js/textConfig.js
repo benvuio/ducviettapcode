37c629b436b190ff7eb37d6b8ed3911966e1f8c2
Nguyen Duc Viet <ngduvie@Mac-5s.local>
2025-03-06 23:38:05 +0700

    Fix lỗi textConfig not defined

---
+++ js/textConfig.js
@@ -0,0 +1,15 @@
+const textConfig = {
+    text1: "Hế lu cô gái đến từ Đồng Tháp!",
+    text2: "Trắc nghiệm zui nhaa :)) ",
+    text3: "Hôm nay có đi chơi 8/3 không nào :D",
+    text4: "Nếu hông trả lời mà thoát ra thì sẽ bị phạt nhé",
+    text5: "Có",
+    text6: "Hông",
+    text7: "Có gì muốn nói với anh hả?",
+    text8: "Gửi câu trả lời ở đây",
+    text9: "Chắc là đang chửi thầm :))",
+    text10: "8/3 cũng chỉ là một ngày thôi, còn em thì ngày nào cũng xứng đáng được trân trọng.",
+    text11: "Hẹn em một ngày không xa mình gặp :D",
+    text12: "Okiii",
+  };
+  
