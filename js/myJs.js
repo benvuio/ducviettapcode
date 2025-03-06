77b7b3e4083e0a4701086975057c0dfc319cbeef
Nguyen Duc Viet <ngduvie@Mac-5s.local>
2025-03-06 23:07:16 +0700

    Cập nhật nội dung câu hỏi

--- js/myJs.js
+++ js/myJs.js
@@ -1,16 +1,16 @@
 const textConfig = {
-  text1: "Hế lu người đẹp bận rộn!",
-  text2: "Trắc nghiệm zui nha bà Trân :)) ",
-  text3: "Bà Trân hum nay có bận hok zậy :D",
-  text4: "Nếu hok trả lời mà thoát ra thì sẽ bị phạt nhé",
-  text5: "Coá",
-  text6: "Hok",
-  text7: "Bà đang nghĩ gì zậy bà Trân?",
+  text1: "Hế lu cô gái đến từ Đồng Tháp!",
+  text2: "Trắc nghiệm zui nhaa :)) ",
+  text3: "Hôm nay có đi chơi 8/3 không nào :D",
+  text4: "Nếu hông trả lời mà thoát ra thì sẽ bị phạt nhé",
+  text5: "Có",
+  text6: "Hông",
+  text7: "Có gì muốn nói với anh hả?",
   text8: "Gửi câu trả lời ở đây",
   text9: "Chắc là đang chửi thầm :))",
-  text10: "Cảm ơn em đã coi bản đồ sao cho anh",
-  text11:
-    "Nay Chủ Nhật, hẹn em qua tuần mình gặp :D",
+  text10: "8/3 cũng chỉ là một ngày thôi, còn em thì ngày nào cũng xứng đáng được trân trọng. Không chỉ vì hôm nay đặc biệt, mà vì em vốn dĩ đã xứng đáng. Ít nhất là với anh",
+
+    "Hẹn em một ngày không xa mình gặp :D",
   text12: "Okiii",
 };
 
