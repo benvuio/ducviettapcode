ae66e60f9fcd035e9bfa3c893983fabe78d3f9f4
Nguyen Duc Viet <ngduvie@Mac-5s.local>
2025-03-07 14:38:50 +0700

    abc

--- js/textConfig.js
+++ js/textConfig.js
@@ -1,15 +1,15 @@
 const textConfig = {
-    text1: "Hế lu cô gái đến từ Đồng Tháp!",
-    text2: "Trắc nghiệm zui nhaa :)) ",
+    text1: "Đồng Tháp có gì đẹp? À đúng rồi, có em",
+    text2: "Trắc nghiệm zui nhaaa",
     text3: "Hôm nay có đi chơi 8/3 không nào :D",
     text4: "Nếu hông trả lời mà thoát ra thì sẽ bị phạt nhé",
-    text5: "Có",
-    text6: "Hông",
+    text5: "Kó",
+    text6: "Có",
     text7: "Có gì muốn nói với anh hả?",
     text8: "Gửi câu trả lời ở đây",
     text9: "Chắc là đang chửi thầm :))",
-    text10: "8/3 cũng chỉ là một ngày thôi, còn em thì ngày nào cũng xứng đáng được trân trọng.",
+    text10: "8 tháng 3 đến thật nhanh. Chúc em vui vẻ, an lành, bình yên.",
     text11: "Hẹn em một ngày không xa mình gặp :D",
-    text12: "Okiii",
+    text12: "Bấm vào đây tức là đồng ý :))",
   };
   
