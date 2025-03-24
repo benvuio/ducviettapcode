f990376eb7e10d8da57028a4661b09d52c899dc9
Nguyen Duc Viet <ngduvie@Mac-5s.local>
2025-03-24 21:06:57 +0700

    Update code

--- js/textConfig.js
+++ js/textConfig.js
@@ -1,7 +1,7 @@
 const textConfig = {
-    text1: "Đồng Tháp có gì đẹp? À đúng rồi, có em",
+    text1: "Chào Béo, tính ghi thêm nhưng không biết ghi gì ",
     text2: "Trắc nghiệm zui nhaaa",
-    text3: "Hôm nay có đi chơi 8/3 không nào :D",
+    text3: "Béo có nhớ anh hok",
     text4: "Nếu hông trả lời mà thoát ra thì sẽ bị phạt nhé",
     text5: "Kó",
     text6: "Cũng là Kó nhưng màu đỏ",
@@ -9,7 +9,7 @@ const textConfig = {
     text8: "Gửi câu trả lời ở đây",
     text9: "Chắc là đang chửi thầm :))",
     text10: "Công nhận làm cái này hơi mất thời gian, nhưng mà nếu đổi lại được nụ cười của em thì cũng đáng đấy chứ, chỉ là không biết em có cười không :))",
-    text11: "8/3 vui vẻ nhen cô gái, hẹn em một ngày không xa mình gặp :D",
+    text11: "Mai có muốn gặp anh hong :D",
     text12: "Bấm vào đây tức là đồng ý :))",
   };
   
